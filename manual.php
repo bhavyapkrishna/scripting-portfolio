427fc3881ad425f95e46fc0deddbeaca7f298cfc
bhavyapk <bhavyapkrishna@gmail.com>
2024-04-24 00:51:54 -0400

    added ability to choose specific manual and section from manual to display

--- manual.php
+++ manual.php
@@ -1,48 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Scripting Manual</h1>
-    <form name="script-form" action="" method="get">
-        <p>Enter the manual you want to access: </p>
-        <input type="text" name="script" id="script">
-    </form>
+    <p>Input the manual you want to access (ex. bash, grep, seds), and the section within the manual you would like to read.</p>
+    <p>This program will output the specific section of the manual you need!</p>
     <br>
-    <form name="function-form" action="" method="get">
-        <p>Enter the function you want to access: </p>
-        <input type="text" name="function" id="function">
+    <form name="script-form" action="" method="get">
+        <p>Enter the manual you want to access: </p> <input type="text" name="script" id="script">
+
+        <p>Enter the function you want to access: </p> <input type="text" name="function" id="function">
+
+        <br>
+        <br>
+        <input type="submit" value="Submit">
+        <br>
     </form>
 
-   <?php 
-   $file = file_get_contents("https://www.man7.org/linux/man-pages/man1/bash.1.html");
+    <?php
 
-   $dom = new DOMDocument();
-   $dom->loadHTML($file);
+    $directory = file_get_contents("https://man7.org/linux/man-pages/dir_all_by_section.html");
+    $scriptDOM = new DOMDocument();
+    $scriptDOM->loadHTML($directory);
 
-   if(isset($_GET['function'])) {
-    $function = $_GET['function'];
-   } else {
-    $function = '';
-   }
+    if (isset($_GET['script'])) {
+        $script = $_GET['script'];
+    } else {
+        $script = '';
+    }
 
-   $function = str_replace(' ', '_', $function);
-   $function = strtoupper($function);
+    if (substr($script, -4) != '(1p)') {
+        $end = '1';
+    } else {
+        $end = '1p';
+        $script = substr($script, 0, -4);
+    }
 
-   $path = new DOMXPath($dom);
-   $sectionQuery = "//a[@id='$function']/parent::h2/following-sibling::pre[1]";
+    $completeString = $script . '(' . $end . ')';
+    $urlStr = $script . '.' . $end;
 
-   $section = $path->query($sectionQuery);
+    $scriptPath = new DOMXPath($scriptDOM);
+    $scriptQuery = "//a[text()='$completeString']/@href";
 
-   if ($section->length > 0) {
-    echo $function;
-    echo $dom->saveHTML($section->item(0));
-   } else {
-    echo "Invalid section!";
-   }
-   ?> 
+    $file = file_get_contents("https://man7.org/linux/man-pages/man1/$urlStr.html");
+    $titleDOM = new DOMDocument();
+    $titleDOM->loadHTML($file);
+
+    $titlePath = new DOMXPath($titleDOM);
+    $titleQuery = "//title";
+
+    $title = $titlePath->query($titleQuery);
+
+    if ($title->length > 0) {
+        echo 'MANUAL: ' . $title->item(0)->nodeValue;
+        echo "<br>";
+        echo "<br>";
+    } else {
+        echo "Invalid title!";
+        echo "<br>";
+        echo "<br>";
+    }
+    
+    if (isset($_GET['function'])) {
+        $function = $_GET['function'];
+    } else {
+        $function = '';
+    }
+
+    $function = str_replace(' ', '_', $function);
+    $function = strtoupper($function);
+
+    $sectionQuery = "//a[@id='$function']/parent::h2/following-sibling::pre[1]";
+
+    $section = $titlePath->query($sectionQuery);
+
+    if ($section->length > 0) {
+        echo $function;
+        echo "<br>";
+        echo $titleDOM->saveHTML($section->item(0));
+    } else {
+        echo "Invalid section!";
+    }
+    ?>
 </body>
+
 </html>
