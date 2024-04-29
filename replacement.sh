b66908101bac7f4cf0c20e89185c6269c16cc9d5
bhavyapk <bhavyapkrishna@gmail.com>
2024-04-28 21:30:22 -0400

    added comments

--- replacement.sh
+++ replacement.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#count the number of command line arguments
+#if there is a command-line argument, set "replacer" to it
 if [ $# -gt 0 ]; then
 replacer=$1;
 else
 replacer="replacement_text";
 fi
 
+#replace every bolded word with the replacer variable
 curl -s "http://awkscripts.com/datw/out.feds.html" | sed -E "s|<b>(.+)</b>|<b>${replacer}</b>|g" > modified_text.html
